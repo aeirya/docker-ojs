3632b833060bfb1972c9ce93f712a3ee65d455e3
aeirya <aeirya@gmail.com>
2024-04-26 17:55:39 +0330

    add empty child theme

---
+++ themes/tritheme/index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @defgroup plugins_themes_default_child Default theme plugin
+ */
+
+/**
+ * @file plugins/themes/defaultChild/index.php
+ *
+ * Copyright (c) 2014-2016 Simon Fraser University Library
+ * Copyright (c) 2003-2016 John Willinsky
+ * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ *
+ * @ingroup plugins_themes_defaultChild
+ * @brief Wrapper for default child theme plugin.
+ *
+ */
+
+require_once('TrithemeThemePlugin.php');
+
+return new TrithemeThemePlugin();
